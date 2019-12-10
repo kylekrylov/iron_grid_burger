e59eacbbe66bf4951a09e85160c7244f79fa6dd4
kylerylov <55537870+kylerylov@users.noreply.github.com>
2019-12-10 14:03:38 +0300

    add block Testimonials, i po melochi

--- js/Burger_click.js
+++ js/Burger_click.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
         $('.burger,.menu').toggleClass('active');
     });
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+    },
+});
