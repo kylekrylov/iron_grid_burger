76216d8644b870968eeb7b9b112f8f33170a498b
kylerylov <55537870+kylerylov@users.noreply.github.com>
2019-12-13 00:06:52 +0300

    preend

--- js/Burger_click.js
+++ js/Burger_click.js
@@ -5,13 +5,16 @@ $(document).ready(function() {
 });
 
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
 
-    },
-});
+//?
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+
+//     },
+// });
+//?
