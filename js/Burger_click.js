c2837c79ad6c8da4e2772f4d3c63c3f590823f18
kylerylov <55537870+kylerylov@users.noreply.github.com>
2019-12-14 12:30:07 +0300

    meta_for_ss

--- js/Burger_click.js
+++ js/Burger_click.js
@@ -4,17 +4,16 @@ $(document).ready(function() {
     });
 });
 
-
-
 //?
-// var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-
-//     },
-// });
+//!
 //?
+
+
+
+let line = document.querySelector('.team_cards');
+
+let slides = document.querySelectorAll('.team_card');
+let slidesWidth = document.querySelector('.team_card').offsetWidth;
+
+console.log(slidesWidth);
+// см продолжение! https://www.youtube.com/watch?v=vM7zWNn4IPE
